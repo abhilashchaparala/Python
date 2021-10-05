7d4a2f7580b5190a7feb0976c9b3e7a3e6378132
abhilashchaparala <abhilashchaparala81@gmail.com>
2021-10-06 01:32:25 +0530

    initial

--- basic prog/as.py
+++ basic prog/as.py
@@ -1,1 +1,15 @@
-# Function for findin
+# Function to sort Python Dictionaries by Key
+
+def sort_dictionaries(dictionary):
+    """
+    Sorts a dictionary by its keys.
+    """
+    return sorted(dictionary.items(), key=lambda x: x[0])
+# Function to sort Python Dictionaries by value
+
+def sort_dictionaries_by_value(dictionary):
+    """
+    Sorts a dictionary by its values.
+    """
+    return sorted(dictionary.items(), key=lambda x: x[1])
+# Function to sort Python Dictionaries by Key and Value
